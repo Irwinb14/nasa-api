138379e5ae7c11e59b39a340838fc936f55b7dd5
Bryan <irwinb14@gmail.com>
2023-09-26 14:14:27 -0700

    analytics component added

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import APODViewer from '../../components/APOD';
 import { News_Cycle } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const newsCycle = News_Cycle({ weight: '400', subsets: ['latin'] });
 
@@ -10,6 +11,7 @@ export default function Home() {
     <div className={newsCycle.className}>
       <div className='HomeCard'>
         <APODViewer />
+        <Analytics />
       </div>
     </div>
   );
